cb43471895a558c36bea88398e17efb150d43937
Kyle West <kyle.west@familysearch.org>
2017-11-10 15:58:57 -0700

    PASSED: DataBase data retrieval

--- lite-web-db.js
+++ lite-web-db.js
@@ -16,7 +16,13 @@ function lwDataBase (path, name, isNew) {
   this.name = name;
   this.path = `${path}/${name}.json`;
   if (isNew) {
-    this.data = {};
+    this.data = {
+      tables: {
+        test1: {
+          rows: []
+        }
+      }
+    };
   } else {
     this.data = require(this.path);
   }
@@ -31,8 +37,83 @@ lwDataBase.prototype = {
   /****************************************************************************
   * Meta Data
   ****************************************************************************/
-  getData: function () {
-    return {};
+  getTableData: function (table, testSet) {
+    var matches = [];
+    var tableData = this.data.tables[table];
+
+    if (!table || !tableData) {
+      throw `Table "${table}" is not found in the ${this.name} Database`;
+    }
+
+    var self = this;
+    switch (typeOf(testSet)) {
+      case "Object":
+        tableData.rows.forEach((row) => {
+          if (self.testRow(row, testSet)) matches.push(row);
+        });
+        return matches;
+        break;
+
+      case "Array":
+        tableData.rows.forEach((row) => {
+          var match = true;
+
+          testSet.forEach((test) => {
+            match = match && self.testRow(row, test);
+          });
+
+          if (match) matches.push(row);
+        });
+        return matches;
+        break;
+
+      default:
+        if (tableData) {
+          return this.data.tables[table].rows;
+        }
+    }
+  },
+
+  testRow: function (row, test) {
+    if (!test || !row) {
+      return false;
+    }
+
+
+
+    if (test.is) {
+      if (!test.key) {
+        throw "IS Tests are required to have a key to compare";
+      } else {
+        if (test.not) {
+          return row[test.key] !== test.is;
+        } else {
+          return row[test.key] === test.is;
+        }
+      }
+    } else if (test.includes && typeOf(test.includes) === "Array") {
+      if (!test.key) {
+        throw "INCLUDES Tests are required to have a key to compare";
+      } else {
+        if (test.not) {
+          return !test.includes.includes(row[test.key]);
+        } else {
+          return test.includes.includes(row[test.key]);
+        }
+      }
+    } else if (test.predicate && typeOf(test.predicate) === "Function") {
+      if (!test.key) {
+        throw "PREDICATE Tests are required to have a key to compare";
+      } else {
+        if (test.not) {
+          return !test.predicate(row[test.key]);
+        } else {
+          return test.predicate(row[test.key]);
+        }
+      }
+    } else {
+      return true;
+    }
   }
 };
 
--- test/test-suites.js
+++ test/test-suites.js
@@ -34,29 +34,50 @@ ${JSON.stringify(db2,null,2)}
 /****************************************************************************
 * Test the data retieval
 ****************************************************************************/
-const search0 = {};
-const search1 = { key: "index", is: 1 };
-const search2 = { key: "index", includes: [1,4] };
-const search3 = { key: "index", predicate: function (val) {
+const search0a = null;
+const search0b = {};
+const search1a = { key: "index", is: 1 };
+const search1b = { key: "index", is: 1, not: true };
+const search2a = { key: "index", includes: [1,4] };
+const search2b = { key: "index", includes: [1,4], not: true };
+const search3a = { key: "index", predicate: function (val) {
   return (val % 2 == 0);
 }};
+const search3b = { key: "index", predicate: function (val) {
+  return (val % 2 == 0);
+}, not: true};
 const search4 = [
   { key: "spiritAnimal", is: "bear" },
-  { key: "password",     is: "bearsR0ck" }
+  { key: "password",     is: "bearsR0ck", not: true }
 ];
 console.log(`
-TESTING SEARCH DATABASE for "${JSON.stringify(search1)}"
+TESTING SEARCH DATABASE
 ---------------------------------------------
+(NULL TEST)
+${JSON.stringify(db1.getTableData("test1",search0a))}
+
+(EMPTY TEST)
+${JSON.stringify(db1.getTableData("test1",search0b))}
+
 WHERE index = 1
-${JSON.stringify(db1.getData(search0))}
+${JSON.stringify(db1.getTableData("test1",search1a))}
+
+WHERE NOT index = 1
+${JSON.stringify(db1.getTableData("test1",search1b))}
 
 WHERE index IN (1,4)
-${JSON.stringify(db1.getData(search1))}
+${JSON.stringify(db1.getTableData("test1",search2a))}
 
-WHERE isEven(index)
-${JSON.stringify(db1.getData(search2))}
+WHERE NOT index IN (1,4)
+${JSON.stringify(db1.getTableData("test1",search2b))}
 
-WHERE spiritAnimal = "bear" AND password = "bearsR0ck"
-${JSON.stringify(db2.getData(search3))}
+WHERE isEven(index) <== {predicate function}
+${JSON.stringify(db1.getTableData("test1",search3a))}
+
+WHERE NOT isEven(index) <== {predicate function}
+${JSON.stringify(db1.getTableData("test1",search3b))}
+
+WHERE spiritAnimal = "bear" AND NOT password = "bearsR0ck"
+${JSON.stringify(db1.getTableData("test1",search4))}
 ---------------------------------------------
 `);
