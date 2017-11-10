cf45df6f1b98606999170cace8fadd42946077c7
Kyle West <kyle.west@familysearch.org>
2017-11-10 11:33:47 -0700

    Wrote startup tests

--- test/test-suites.js
+++ test/test-suites.js
@@ -1,10 +1,27 @@
 const lwdb = require('../lite-web-db.js');
-const db1  = 'main';
-const db   = lwdb.startup(db1);
-
 console.log(`
-LOADED DB from ${db1}
+LOADED DATABASE SYSTEM
 ---------------------------------------------
-${JSON.stringify(db,null,2)}
+${JSON.stringify(lwdb,null,2)}
+---------------------------------------------
+`);
+
+
+const db1n = 'test';
+const db1  = lwdb.startup(db1n);
+console.log(`
+TESTING LOADING AN EXISITING DATABASE "${db1n}"
+---------------------------------------------
+${JSON.stringify(db1,null,2)}
+---------------------------------------------
+`);
+
+
+const db2n = 'main';
+const db2  = lwdb.startup(db2n);
+console.log(`
+TESTING CREATING A NEW DATABASE "${db2n}"
+---------------------------------------------
+${JSON.stringify(db2,null,2)}
 ---------------------------------------------
 `);
